86ed1397d24daa09c7df7aae046ec4eae014033d
Nasir Idrishi <nasiridrishi@outlook.com>
2022-01-05 20:15:43 +0530

    Version 3.0.4

--- src/main/java/net/primegames/survival/PrimesSurvival.java
+++ src/main/java/net/primegames/survival/PrimesSurvival.java
@@ -3,6 +3,7 @@ package net.primegames.survival;
 import net.primegames.core.PrimesCore;
 import net.primegames.core.gamemode.GameMode;
 import net.primegames.core.gamemode.GameModeId;
+import net.primegames.survival.listener.MineTickerListener;
 import net.primegames.survival.listener.SurvivalGroupListener;
 import org.bukkit.plugin.PluginManager;
 
@@ -25,5 +26,6 @@ public class PrimesSurvival extends GameMode {
     public void registerEvents() {
         PluginManager manager = PrimesCore.getInstance().getServer().getPluginManager();
         manager.registerEvents(new SurvivalGroupListener(), PrimesCore.getInstance());
+        manager.registerEvents(new MineTickerListener(), PrimesCore.getInstance());
     }
 }
